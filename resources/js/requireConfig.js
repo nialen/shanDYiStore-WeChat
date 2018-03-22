60260dae5afcdc33ec02534bd7b67948d969a080
聂亚兰 <3242662985@qq.com>
2018-03-22 09:48:07 +0800

    feat：js,css,html

---
+++ resources/js/requireConfig.js
@@ -0,0 +1,53 @@
+/**
+ * 公共的require配置
+ */
+
+require.config({
+	baseUrl: '../../resources/js',
+	urlArgs: 'v=20180304',
+	paths: {
+		angular: './angular.min',
+		'angular-animate': './angular-animate.min',
+		jquery: './jquery.min',
+		lodash: './lodash.min',
+		mock: './mock',
+		select: './select',
+		jqueryDialog: './jquery.dialog',
+		ngJqueryDialog: './ngJqueryDialog',
+		httpServer: './httpServer',
+		httpMethod: './httpMethod',
+	},
+	shim: {
+		angular: {
+			exports: 'angular'
+		},		
+		'angular-animate': {
+			deps: ['angular'],
+			exports: 'ngAnimate'
+		},		
+		select: {
+			deps: ['angular']
+		},
+		ngJqueryDialog: {
+			deps: ['angular', 'jquery', 'jqueryDialog']
+		},		
+		httpServer: {
+			deps: ['angular'],
+			exports: 'httpServer'
+		},
+		httpMethod: {
+			deps: ['angular', 'httpServer'],
+			exports: 'httpMethod'
+		}
+	}
+});
+
+require(['jquery'], function($) {
+	var currentPage = $('#page').attr('current-page');
+	var targetModule = $('#page').attr('target-module');
+
+	// mockData 加载与否决定是否启用模拟数据
+	require(['angular', currentPage], function(angular) {
+		angular.bootstrap(document, [targetModule]);
+	});
+});
---
+++ resources/js/workReport.js
@@ -0,0 +1,8 @@
+define(['angular', 'jquery', 'lodash', 'mock', 'select', 'httpMethod', 'angular-animate'], function(angular, $, _, Mock) {
+	angular
+		.module('workReportModule', ['ui.select', 'httpMethod'])
+
+
+
+
+});
